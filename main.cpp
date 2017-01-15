bca983d774ea980f03fbb8ce73ad42301c6b65a9
Augusta Dunca <dcaugusta@gmail.com>
2017-01-15 16:06:02 +0200

    Exercitiul 1- suma sir

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int a[20], nr, i;
+
+    cout << "Numarul de elemente din sirul a (maximum 20) ";
+    cin >> nr;
+    for (i = 0; i < nr; i++) {
+        cout << "a[" << i << "] = ";
+        cin >> a[i];
+    }
+    long sum ;
+
+    for (i = 0, sum = 0; i < nr; i++){
+        sum=sum+a[i];
+    }
+    cout << "Suma elementelor este " << sum << endl;
+
+   return 0;
+}
